424909369b3c156d7ac83fa9c81d05d6e817fd8b
SunM <coex0113@gmail.com>
2024-07-12 12:10:54 +0900

    feat. codingapple-06-import,export

--- vuejs-framework/vuedongsan/README.md
+++ vuejs-framework/vuedongsan/README.md
@@ -30,6 +30,13 @@ npm run lint
 - UI의 상태를 명시해주는 변수를 `state` 라고 함.
 - 동적으로 이미지 경로를 주기 위해서는 require 또는 문자열 템플릿을 사용해야 함.
 
+### 06-Import, Export 문법
+1. export할 파일에 default는 전체, 여러개일 경우는 {} 사용
+
+- import 문법을 사용할 때 from에서 js는 생략해도 무관
+- html 태그안에 데이터 바인딩은 :, html 태그 밖에 데이터 바인딩 {{}}
+- 주석처리 : script에서 import 구문, component 구문, </> 구문 
+
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- vuejs-framework/vuedongsan/src/App.vue
+++ vuejs-framework/vuedongsan/src/App.vue
@@ -2,19 +2,22 @@
   <div style="text-align: center;">
     https://www.youtube.com/watch?v=T4N9wjx7_E4&list=PLfLgtT94nNq3Br68sEe26jkOqCPK_8UQ-&index=4
   </div>
-  <event-handler></event-handler>
-  <app-modal></app-modal>
+  <!-- <event-handler></event-handler> -->
+  <!-- <app-modal></app-modal> -->
+  <import-export></import-export>
 </template>
 
 <script>
-import EventHandler from "./components/EventHandler.vue"
-import AppModal from './components/AppModal.vue';
+// import EventHandler from "./components/EventHandler.vue"
+// import AppModal from './components/AppModal.vue';
+import ImportExport from './components/ImportExport.vue';
 
 export default {
   name: 'App',
   components: {
-    EventHandler,
-    AppModal,
+    // EventHandler,
+    // AppModal,
+    ImportExport,
   },
   data() {
     return {};
