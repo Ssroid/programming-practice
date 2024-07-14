e19152c9b5104342091aecc62ee113e232feeaad
SunM <coex0113@gmail.com>
2024-07-14 20:39:55 +0900

    feat. vue3-full-cource-5.1 이벤트 핸들러

---
+++ vuejs-framework/vue-full-course/src/App_4.4.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- v-for 디렉티브 사용 -->
+    <li v-for="(item, index) in samepleArray" :key="item" @click="openDialog(index)">{{ item }} / {{ index }}</li>
+    <li v-for="(user, index) in otherArray" :key="user.name">{{ user.id }} / {{ index }}</li>
+
+    <!-- 기전 li 태그 반복 -->
+    <!-- <li>{{ samepleArray[0] }}</li> -->
+    <!-- <li>{{ samepleArray[1] }}</li> -->
+    <!-- <li>{{ samepleArray[2] }}</li> -->
+    <!-- <li>{{ samepleArray[3] }}</li> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    
+
+    return {
+      samepleArray: ['a', 'b', 'c', 'd'],
+      otherArray: [
+        { id: 0, name: 'John'},
+        { id: 1, name: 'Kim'},
+        { id: 2, name: 'Lee'},
+        { id: 3, name: 'Park'},
+      ],
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vuejs-framework/vue-full-course/src/App_5.1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="count++">인라인 핸들러</button>
+    <h1>{{ count }}</h1>
+
+    <button v-on:click="changeName">메소드 핸들러</button>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    
+    return {
+      count: 0,
+      name: 'Vue.js',
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = this.name + '!';
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
