cd479a86a96190de6fe04efa42dd2ac2084bf2e8
SunM <coex0113@gmail.com>
2024-07-14 20:58:58 +0900

    feat. vue3-full-cource-5.2 Computed 이해

--- vuejs-framework/vue-full-course/src/App.vue
+++ vuejs-framework/vue-full-course/src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-
-  </div>
+  <h1>{{ text }}</h1>
+  <h1>changeText 함수 호출 값: {{ changeText() }}</h1>
+  <h1>changeText 함수 호출 값: {{ changeText() }}</h1>
+  <h1>changeText 함수 호출 값: {{ computedText }}</h1>
+  <h1>changeText 함수 호출 값: {{ computedText }}</h1>
 </template>
 
 <script>
 export default {
   data () {
-    
 
-    return {}
+    return {
+      text: "Computed 테스트 데이터 문구입니다.",
+    }
+  },
+  computed: {
+    computedText() {
+      console.log("computedText 함수 호출");
+      return this.text.split('').reverse().join('');
+    },
+  },
+
+  methods: {
+    changeText() {
+      console.log("함수 호출");
+      console.log(this.text);
+
+      return this.text.split('').reverse().join('');
+    },
   }
 }
 </script>
